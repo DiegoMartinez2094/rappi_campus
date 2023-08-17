7a09bfc43d72b838a65e2efd6014609479f1641d
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-17 00:22:21 -0500

    feat: :sparkles: archivo de iniciacion de app
    punto de inicio de principal

---
+++ app.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+import appCliente from "./routers/Cliente.js"
+dotenv.config();
+let app=express();
+
+app.use(express.json());
+
+let config = JSON.parse(process.env.My_server);
+app.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`);
+});
+
+app.use("/clientes",appCliente);
