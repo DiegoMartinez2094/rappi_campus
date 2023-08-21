4ca2b2dffb7d730f6205a29aefe46a9168984842
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-20 20:08:54 -0500

    feat: :sparkles: implementacion de la funcion ordenes
    en esta funcion se implementa la busqueda de una orden, actualizacion y eliminacion segun el requerimiento

---
+++ funcion/Ordenes.js
@@ -0,0 +1,81 @@
+import { con } from "../db/atlas.js";
+import { Router } from "express";
+
+const ordenes = Router();
+
+ordenes.get("/todas", async (req, res) => {
+  try {
+    const db = await con();
+    const ordenes = db.collection("ordenes");
+    const result = await ordenes.find({}).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error al obtener los ordenes:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+// Realizar la consulta por el idOrden
+ordenes.get("/:idOrden", async (req, res) => {
+  try {
+    const idOrden = parseInt(req.params.idOrden); // Convertir el parámetro a número entero
+    const db = await con();
+    const ordenes = db.collection("ordenes");
+
+    const result = await ordenes.findOne({ idOrden });
+
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(404).send("Orden no encontrada");
+    }
+  } catch (error) {
+    console.error("Error al obtener la orden:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+// Eliminar una orden por idOrden
+ordenes.delete("/:idOrden", async (req, res) => {
+  try {
+    const idOrden = parseInt(req.params.idOrden); // Convertir el parámetro a número entero
+    const db = await con();
+    const ordenes = db.collection("ordenes");
+
+    // Eliminar la orden por idOrden
+    const result = await ordenes.deleteOne({ idOrden });
+
+    if (result.deletedCount === 1) {
+      res.send("Orden eliminada exitosamente");
+    } else {
+      res.status(404).send("Orden no encontrada");
+    }
+  } catch (error) {
+    console.error("Error al eliminar la orden:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+// Actualizar una orden por idOrden
+ordenes.put("/:idOrden", async (req, res) => {
+  try {
+    const idOrden = parseInt(req.params.idOrden); // Convertir el parámetro a número entero
+    const updatedData = req.body; // Los datos actualizados se esperan en el cuerpo del request
+    const db = await con();
+    const ordenes = db.collection("ordenes");
+
+    // Actualizar la orden por idOrden
+    const result = await ordenes.updateOne({ idOrden }, { $set: updatedData });
+
+    if (result.matchedCount === 1) {
+      res.send("Orden actualizada exitosamente");
+    } else {
+      res.status(404).send("Orden no encontrada");
+    }
+  } catch (error) {
+    console.error("Error al actualizar la orden:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+export default ordenes;
