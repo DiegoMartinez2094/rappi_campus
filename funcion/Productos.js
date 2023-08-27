a4c2f32f0d1a9c37cb1db126b2a89a6f3996858c
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-27 00:07:38 -0500

    feat: :sparkles: Dto para la coleccion productos
    se implementa el dto con la librería express-validator para la coleccion productos

--- funcion/Productos.js
+++ funcion/Productos.js
@@ -1,12 +1,11 @@
 import { con } from "../db/atlas.js";
 import { Router } from "express";
-
+import { validationResult } from "express-validator";
 
 const producto = Router();
-
 const db = await con();
 
-producto.get("/disponibles", async(req, res) => {
+producto.get("/", async(req, res) => {
     try {
         const producto = db.collection("productos");
         const result = await producto.find({ cantidad: { $gte: 1 } }).toArray();
@@ -17,8 +16,28 @@ producto.get("/disponibles", async(req, res) => {
     }
 });
 
-producto.post("/nuevos", async(req, res)=>{
+producto.get("/:id", async (req, res) => {
+    const id = parseInt(req.params.id); // Parsea el parámetro como un número entero
+    try {
+        const db = await con();
+        const productos = db.collection("productos");
+        const result = await productos.findOne({ id }); 
+        if (result) {
+            res.send(result);
+        } else {
+            res.status(404).send("producto no encontrado");
+        }
+    } catch (error) {
+        console.error("Error al obtener el producto:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
 
+producto.post("/", async(req, res)=>{
+    const {errors} = validationResult(req)
+    if (errors.length > 0) {
+        return res.status(400).json({ errors: errors });
+      }
     let result;
     try {
         const producto = db.collection("productos");
@@ -32,10 +51,12 @@ producto.post("/nuevos", async(req, res)=>{
 });
 
 producto.put("/:id", async(req, res)=>{
-
+    const {errors} = validationResult(req)
+    if (errors.length > 0) {
+        return res.status(400).json({ errors: errors });
+      }
     const id = parseInt(req.params.id);
     const newData = req.body; 
-
     try {
         const productos = db.collection("productos");
         const result = await productos.updateOne({ id }, { $set: newData });
@@ -49,13 +70,10 @@ producto.put("/:id", async(req, res)=>{
         console.error("Error al actualizar el producto:", error);
         res.status(500).send("Error interno del servidor");
     }
-
 });
 
 producto.delete("/:id", async(req, res)=>{
-
     const id = parseInt(req.params.id);
-
     try {
         const producto = db.collection("productos");
         const result = await producto.deleteOne({ id });
